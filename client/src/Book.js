c8e3a0ced09e538391e927f6f9929750c1e30567
Dogancan94 <dogancankinik@gmail.com>
2021-11-20 17:54:35 +0300

    Book Component

---
+++ client/src/Book.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Book.css'
+
+function Book(){
+    return (
+        <div className='book-message'>
+            Hello I'm a book!
+        </div>    
+    );
+}
+
+export default Book;
